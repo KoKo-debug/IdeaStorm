1cd325f6ffc3b7dac44161aa14fc05d1520356ec
Zhiyuan He <zh2312@columbia.edu>
2019-12-31 10:59:14 -0500

    Create Group model and modify User model

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const db = require("./conf/keys").mongoURI;
 const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const User = require('./models/User');
+const Group = require('./models/Group');
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -24,7 +26,29 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    User.findOne({ email: 'test001@mail.com' })
+        .then(user => {
+            const newGroup = new Group({
+                name: "test group 001",
+                creator: user,
+                joinCode: "abcd1234"
+            });
+            newGroup.members.push(user);
+            newGroup.save().then(
+                group => {
+                    user.createdGroups.push(group);
+                    user.save().then(user => {
+                        user.populate('createdGroups');
+                        res.json(user);
+                        }
+                    );
+                }
+            )
+        })
+})
+
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);    
+    console.log(`Listening to port ${port}`);
 });
 
