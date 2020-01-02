c979fbbe1f1c681dfa5fe0f59fb2c4bc8627a0de
App Academy Student <email@email.com>
2020-01-02 11:23:06 -0500

    edit package.json

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -14,12 +22,6 @@ const Group = require('./models/Group');
 const Board = require('./models/Board');
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
