55bfe7c65bd39663f232e83f4739eccbf5fae466
App Academy Student <email@email.com>
2020-01-02 10:29:20 -0500

    board elements for group index

---
+++ frontend/src/components/main/group_boards.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class BoardIndex extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  buildBoard(key){
+    return(
+      <li className="board-list-element">{key}</li>
+    )
+  }
+
+  render(){
+    let boards = this.props.boards;
+
+    let boardEles = Object.keys(boards).map((key, idx) => {
+      return(
+        // this.buildBoard(key)
+        <li key={idx}>
+          {boards[key].title}
+        </li>
+      )
+    });
+    
+    return(
+      <ul className="group-board-container">
+        {boardEles}
+      </ul>
+    )
+  }
+}
+
+export default BoardIndex;
