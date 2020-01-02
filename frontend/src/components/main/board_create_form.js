045d4330a15cd32a59159ce3fbbe109cc9877779
App Academy Student <email@email.com>
2020-01-02 17:33:19 -0500

    create board button styling

--- frontend/src/components/main/board_create_form.js
+++ frontend/src/components/main/board_create_form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import plusIcon from "./main_images/plusIcon.jpeg";
 
 const testFunc = () => {
 
@@ -9,17 +10,34 @@ class BoardCreateForm extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-
+      hoverText: false
     };
+  this.buttonText = this.buttonText.bind(this);
+  }
+
+  buttonText(){
+    if(this.state.hoverText){
+      return(
+        <div>
+          Create<br />
+          New<br />
+          Board
+        </div> 
+      )
+    } else {
+      return(
+        <div>
+          <img src={plusIcon} className="create-board-plusicon"></img>
+        </div>
+      )
+    }    
   }
 
   createButton(){
     return(
       <div className="gi-board-button-container">
         <button className="gi-board-create-button">
-          Create<br />
-          New<br />
-          Board
+          { this.buttonText() }           
         </button>
       </div>
       
