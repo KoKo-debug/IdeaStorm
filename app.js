7f2f53c3c2358b66259c1e9bbc6ea53abba42c01
App Academy Student <email@email.com>
2020-01-02 10:42:28 -0500

    deploy to heroku

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ const User = require('./models/User');
 const Group = require('./models/Group');
 const Board = require('./models/Board');
 
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("connected to mongoDB"))
